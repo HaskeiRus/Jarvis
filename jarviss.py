2b6513c5ce4530e401e0cfe97479c6047318bf2d
HaskeiRus <haskeirus@gmail.com>
2017-07-29 18:26:57 -0400

    Add files via upload

--- jarviss.py
+++ jarviss.py
@@ -3,16 +3,53 @@
 from datetime import datetime
 from google import search
 import webbrowser
-print('As perguntas que podem ser feitas são: Que horas são? Qual a data de hoje? Escreva (Pesquise por) e depois ira aparecer para voce escrever oque vc quer pesquisar  ')
+import pyttsx
+en = pyttsx.init()
+now = datetime.now()
+en.say('Ola, tudo bem?')
+en.setProperty('voice', b'brazil')
+en.runAndWait()
+print("")
+en.say("Escreva 'Oque você pode fazer?' e vera oque posso fazer")
+en.setProperty('voice', b'brazil')
+en.runAndWait()
+hora = str(now.hour)
+minutos = str(now.minute)
+dia = str(now.day)
+mes = str(now.month)
+ano = str(now.year)
 
+en.say(hora)
+en.runAndWait()
 while True:
-  now = datetime.now()
+
   quest = input('Você: ')
 
+  if quest == "Oque você pode fazer?":
+    en.say('As perguntas que podem ser feitas são:')
+    en.say(' Que horas são? Qual a data de hoje? Escreva (Pesquise por) e depois ira aparecer para voce escrever oque vc quer pesquisar  ')
+    en.setProperty("voice", b'brazil')
+    en.runAndWait()
+
   if quest == "Que horas são?":
-    print("são: ", now.hour, ':', now.minute)
+    en.say(hora)
+    en.say("e")
+    en.say(minutos)
+    en.setProperty('voice', b'brazil')
+    en.runAndWait()
   elif quest == 'Qual a data de hoje?':
-    print('é', now.day, 'de', now.month, 'de', now.year)
+    en.say(dia)
+    en.say("de")
+    en.say(mes)
+    en.say("De")
+    en.say(ano)
+    en.setProperty('voice', b'brazil')
+    en.runAndWait()
   elif quest == "pesquise por":
     pp = input('pesquisar por: ')
+    pesquisa = str(pp)
+    en.say("Pesquisando por")
+    en.say(pesquisa)
+    en.setProperty("voice", b'brazil')
+    en.runAndWait()
     webbrowser.open('https://www.google.com.br/webhp#q='+ pp, autoraise=True )
